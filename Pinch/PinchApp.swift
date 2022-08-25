f8819d8c7b86cc231ba2ffa3540ca7fd1e57081e
Manishsurti <109143099+Manishsurti13@users.noreply.github.com>
2022-08-25 15:03:06 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Manish on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
